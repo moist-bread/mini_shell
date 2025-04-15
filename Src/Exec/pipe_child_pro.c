5fff22a940c06eb45642f24848f06b548ca53fa5
moist-bread <raquelgo2work@gmail.com>
2025-04-15 18:49:51 +0100

    Add environment duplication

--- Src/Exec/pipe_child_pro.c
+++ Src/Exec/pipe_child_pro.c
@@ -10,30 +10,30 @@ void assign_pipe_fds(t_minishell minishell, t_pipe_data *pipex, int *redir_fd, i
 	{
     	printf("in redir --\n");
 		pipex->cur_pipe[0] = redir_fd[0];
-		if (pipex->next_pipe[0] > 2) // possible problem
-			close(pipex->next_pipe[0]);
+		if (pipex->next_pipe > 2) // possible problem
+			close(pipex->next_pipe);
 	}
 	else // no in redir, input from pipe/stdin if prev cmd was redir out or cur is first
 	{
     	printf("no in redir --\n");
-		pipex->cur_pipe[0] = pipex->next_pipe[0];
+		pipex->cur_pipe[0] = pipex->next_pipe;
 	}
     printf("create pipe --\n"); // standard case
 	if (pipe(new_pipe) == -1)
 		pipex_clean_up(minishell, 1); // pipe didnt create ABORT
 	pipex->cur_pipe[1] = new_pipe[1];
-	pipex->next_pipe[0] = new_pipe[0];
+	pipex->next_pipe = new_pipe[0];
 	if (redir_fd[1] > 2 || idx + 1 == pipex->cmd_n) // out redir or last cmd
 	{
     	printf("out redir or last cmd --\n");
 		close (pipex->cur_pipe[1]);
 		pipex->cur_pipe[1] = redir_fd[1];
 		if (idx + 1 == pipex->cmd_n)
-			close (pipex->next_pipe[0]);
+			close (pipex->next_pipe);
 	}
 	printf("\nAFTER FDS\n\nredir_fd[0]: %d\nredir_fd[1]: %d\n", redir_fd[0], redir_fd[1]);
 	printf("pipex->cur_pipe[0]: %d\npipex->cur_pipe[1]: %d\n", pipex->cur_pipe[0], pipex->cur_pipe[1]);
-	printf("pipex->next_pipe[0]: %d\n", pipex->next_pipe[0]);
+	printf("pipex->next_pipe: %d\n", pipex->next_pipe);
 }
 
 void child_parse_and_exe(t_minishell minishell, t_tree_node *cmd_node, t_pipe_data *pipex)
@@ -68,7 +68,7 @@ char	*get_path(t_minishell minishell, char *cmds)
 	char	*bar;
 	int		i;
 
-	path = ft_split(getenv("PATH"), ':');
+	path = ft_split(get_env("PATH", minishell.env), ':');
 	if (!path)
 		return (ft_strdup(cmds));
 	i = -1;
@@ -86,4 +86,18 @@ char	*get_path(t_minishell minishell, char *cmds)
 		free(joined);
 	}
 	return (free_split(path), ft_strdup(cmds));
+}
+
+char	*get_env(char *search, char **env)
+{
+	int	i;
+
+	i = 0;
+	if (!env)
+		return (NULL);
+	while (env[i] && ft_strncmp(env[i], search, ft_strlen(search)) != 0)
+		i++;
+	if (env[i])
+		return ((env[i] + ft_strlen(search)));
+	return (NULL);
 }
