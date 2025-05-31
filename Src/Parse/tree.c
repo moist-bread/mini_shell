9bbc579970a6f70095de000b45a86cf158b836ca
moist-bread <raquelgo2work@gmail.com>
2025-05-31 16:05:07 +0100

    Small exit print fix

--- Src/Parse/tree.c
+++ Src/Parse/tree.c
@@ -12,11 +12,11 @@ t_tree_node	*create_tree(t_token **tokens, t_minishell *ms)
 	if (expand_token_list(tokens, ms) == 1)
 		exit (-1);
 	assign_type_token(*tokens, true);
-	printf("\nafter expansion:\n");
-	print_tokens(*tokens);
+	//printf("\nafter expansion:\n");
+	//print_tokens(*tokens);
 	if (place_treenode(*tokens, &tree_node, false) == -1)
 		return (NULL);
-	tree_apply_print(tree_node, 0, "Root");
+	//tree_apply_print(tree_node, 0, "Root");
 	return (tree_node);
 }
 
